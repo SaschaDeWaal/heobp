cf591e97183eef1e1afff8f766be5fb604dbb516
Sascha <info@dreamfoxgames.com>
2016-09-19 09:27:09 +0200

    opdracht 2

---
+++ opdracht2/src/particle.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ofMain.h"
+
+class particle
+{
+public:
+	ofPoint position;
+	float radius;
+	ofVec2f speed;
+	long lifeTime;
+
+	ofColor centerColor = ofColor::white;
+	ofColor innerColor;
+	ofColor outerColor;
+
+	particle();
+
+	void Setup();
+	void Move();
+	void Draw();
+
+};
+
